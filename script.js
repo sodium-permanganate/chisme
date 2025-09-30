e502ce4efdf30a752425b20734ee1adeb7925375
sodium-permanganate <diego26007.videojuegos@gmail.com>
2025-09-30 19:19:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var request = new XMLHttpRequest();
 
+request.open('GET', 'https://api.ipdata.co/?api-key=f95f57ca9ab4a07dc5c79a7018898ed8bab943aeb0b825449b050e93');
+
+request.setRequestHeader('Accept', 'application/json');
+
+request.send();
+
+console.log(request.responseText);
